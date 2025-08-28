4e03580357e833cc611d338dd372255280483960
Luka Vasko <egponent@gmail.com>
2025-08-28 12:08:38 -0400

    initial commit

---
+++ lua/plugins/init.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- test new blink
+  { import = "nvchad.blink.lazyspec" },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+  
+  {
+    "aserowy/tmux.nvim",
+    config = function() 
+      require("tmux").setup({})
+    end,
+  },
+  {
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    config = function()
+    require('transparent').setup({})
+  end,
+  }
+}
