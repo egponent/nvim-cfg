e0ba9ba583649e3c2beecd8a925f355dd349b05f
Luka Vasko <egponent@gmail.com>
2025-11-16 15:18:54 -0500

    add codeactions keybind

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,9 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- codeactions keybind
+vim.keymap.set("n", "ca", function()
+  vim.lsp.buf.code_action()
+end, { desc = "LSP Code Action" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
